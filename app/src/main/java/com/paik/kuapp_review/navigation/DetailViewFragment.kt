ea301c5ff506e38c40beeedb9bcdc5b702b4b36f
NachoCookie <sergiopark10373@likelion.org>
2020-05-01 15:43:09 +0900

    Second Commit : Error fixing complete

--- app/src/main/java/com/paik/kuapp_review/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/paik/kuapp_review/navigation/DetailViewFragment.kt
@@ -6,16 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.paik.kuapp_review.R
 import com.paik.kuapp_review.navigation.model.ContentDTO
+import kotlinx.android.synthetic.main.item_detail.view.*
 import java.util.zip.Inflater
 
 class DetailViewFragment : Fragment(){
     var firestore : FirebaseFirestore? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
-        firestore = FirebaseFirestore.getInstance() //firestore 초기화
+        firestore = FirebaseFirestore.getInstance()
         return view
     }
     inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){ 
@@ -34,22 +36,34 @@ class DetailViewFragment : Fragment(){
             }
        }
 
-        override fun onCreateViewHolder(patent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(patent.content).inflate(R.layout.item_detail,p0, false)
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_detail,p0, false)
             return CustomViewHolder(view)
         }
 
-        inner class CustomViewHolder(view: View?) : RecyclerView.ViewHolder() {
-
-        }
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
         override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+            return contentDTOs.size
         }
 
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            TODO("Not yet implemented")
-        }
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var viewholder = (p0 as CustomViewHolder).itemView
 
+            //User Id
+            viewholder.detailviewitem_profile_textview.text = contentDTOs!![p1].userId
+
+            //Image
+            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_imageview_content)
+
+            //Explain of content
+            viewholder.detailviewitem_explain_textview.text = contentDTOs!![p1].explain
+
+            //likes
+            viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![p1].favoriteCount
+
+            //ProfileImage
+            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_profile_image)
+        }
     }
 }
