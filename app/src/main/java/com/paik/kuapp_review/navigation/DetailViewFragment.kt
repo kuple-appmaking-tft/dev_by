538868afa3bb11c1fedafd0923fab020193d467d
NachoCookie <sergiopark10373@likelion.org>
2020-05-01 15:23:41 +0900

    Second Commit : Common Errors Exist

--- app/src/main/java/com/paik/kuapp_review/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/paik/kuapp_review/navigation/DetailViewFragment.kt
@@ -5,12 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.paik.kuapp_review.R
+import com.paik.kuapp_review.navigation.model.ContentDTO
 import java.util.zip.Inflater
 
 class DetailViewFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+        firestore = FirebaseFirestore.getInstance() //firestore 초기화
         return view
+    }
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){ 
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf() 
+        var contentUidList : ArrayList<String> = arrayListOf()
+        init{
+            firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapshot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+       }
+
+        override fun onCreateViewHolder(patent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(patent.content).inflate(R.layout.item_detail,p0, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View?) : RecyclerView.ViewHolder() {
+
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
